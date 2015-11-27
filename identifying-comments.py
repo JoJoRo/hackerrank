b6839af9aed05d8f110c89841704b6971344c313
J J H R <jjhernandezronquillo@gmail.com>
2015-11-27 17:22:37 +0000

    Create identifying-comments.py

---
+++ identifying-comments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+# Identifying comments on ide
+# http://pythex.org/
+import sys
+import re
+
+pattern = '(/\*(?:.|\n)*?\*/)|(//.*)'
+
+comment = re.compile(pattern)
+
+s = ''
+for line in sys.stdin:
+    s += line
+
+words = comment.findall(s)
+
+for i in words:
+	for j in i:
+		if j:
+			# remove spaces after \n
+			aux = j.split('\n')
+			aux = [k.strip() for k in aux if k]
+			for q in aux:
+				print(q)
